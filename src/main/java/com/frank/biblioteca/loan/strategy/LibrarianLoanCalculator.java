83fc9eaf4319091e54525dc8c07ffe45cf59e9ca
frankdrex322 <frankwoker35@gmail.com>
2026-07-06 00:21:28 -0500

    Refactorizado con el vercel

--- src/main/java/com/frank/biblioteca/loan/strategy/LibrarianLoanCalculator.java
+++ src/main/java/com/frank/biblioteca/loan/strategy/LibrarianLoanCalculator.java
@@ -2,9 +2,9 @@ package com.frank.biblioteca.loan.strategy;
 
 import java.math.BigDecimal;
 
-import com.frank.biblioteca.loan.domain.model.Loan;
-import com.frank.biblioteca.loan.domain.model.value_object.LoanCost;
-import com.frank.biblioteca.loan.domain.repository.LoanCalculator;
+import com.frank.biblioteca.loan.models.Loan;
+import com.frank.biblioteca.loan.models.value_object.LoanCost;
+import com.frank.biblioteca.loan.repository.LoanCalculator;
 
 public class LibrarianLoanCalculator implements LoanCalculator {
     private static final BigDecimal DAILY_FEE = new BigDecimal("0.20");
@@ -13,7 +13,9 @@ public class LibrarianLoanCalculator implements LoanCalculator {
     @Override
     public LoanCost calculate(Loan loan) {
         BigDecimal loanFee = DAILY_FEE.multiply(BigDecimal.valueOf(loan.getLoanDurationInDays()));
-        BigDecimal overdueFee = OVERDUE_FEE_PER_DAY.multiply(BigDecimal.valueOf(loan.getOverdueDays(loan.getLoanPeriod().getDueDate())));
+
+        BigDecimal overdueFee = OVERDUE_FEE_PER_DAY.multiply(BigDecimal.valueOf(loan.getOverdueDays(loan.getReturnDate())));
+
         return new LoanCost(loanFee, overdueFee);
     }
     
